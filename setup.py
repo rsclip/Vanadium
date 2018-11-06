be4b63e125af7cc601f8f63e58b2223e1b20bb39
Cyclip <40525455+Cyclip@users.noreply.github.com>
2018-11-06 17:57:36 +0000

    Added setup and /vanadium/ for __init__.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="vanadium",
+    version="0.0.1",
+    author="Cyclip",
+    author_email="gdneop@gmail.com",
+    description="A python module for small things; so you don't have to reinvent the wheel",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Cyclip/Vanadium",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
