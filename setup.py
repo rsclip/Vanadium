2a9fe2fd646b2193919853baea9b86720a3187b4
Cyclip <40525455+Cyclip@users.noreply.github.com>
2018-11-06 18:06:06 +0000

    Updated setup.py
    Included more information

--- setup.py
+++ setup.py
@@ -9,6 +9,9 @@ setuptools.setup(
     author="Cyclip",
     author_email="gdneop@gmail.com",
     description="A python module for small things; so you don't have to reinvent the wheel",
+    maintainer = "Cyclip"
+    maintainer_email = "gdneop@gmail.com"
+
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/Cyclip/Vanadium",
@@ -16,6 +19,22 @@ setuptools.setup(
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+        "Operating System :: OS Independent"
+        "Natural Language :: English
+        "Intended Audience :: Developers"
+        "Developement Status :: 2 - Pre-Alpha",
     ],
+
+    install_requires=[
+        'random'
+        'base64'
+        'hashlib'
+        'threading'
+        'requests'
+        'pathlib'
+        'smtplib'
+        'sys'
+        'inspect'
+        'subprocess'
+        'ctypes'
 )
