7f6f860f59f05dbc26617a199c934bdc079c6e8d
Cyclip <40525455+Cyclip@users.noreply.github.com>
2018-11-08 20:40:30 +0000

    Update 0.0.2.2
    fix update 0.0.2

--- setup.py
+++ setup.py
@@ -5,12 +5,10 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="vanadium",
-    version="0.0.1",
+    version="0.0.2.1",
     author="Cyclip",
     author_email="gdneop@gmail.com",
     description="A python module for small things; so you don't have to reinvent the wheel",
-    maintainer = "Cyclip"
-    maintainer_email = "gdneop@gmail.com"
 
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -20,21 +18,5 @@ setuptools.setup(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent"
-        "Natural Language :: English
-        "Intended Audience :: Developers"
-        "Developement Status :: 2 - Pre-Alpha",
-    ],
-
-    install_requires=[
-        'random'
-        'base64'
-        'hashlib'
-        'threading'
-        'requests'
-        'pathlib'
-        'smtplib'
-        'sys'
-        'inspect'
-        'subprocess'
-        'ctypes'
+    ]
 )
